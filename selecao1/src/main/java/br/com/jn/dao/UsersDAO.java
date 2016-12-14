267b9b47c650dddb50940c4e0f2aa52cc401f263
josmar <josmar@josmar-Inspiron-5537>
2016-12-14 16:33:23 -0200

    funcionando - compartilhamento 7/12

--- selecao1/src/main/java/br/com/jn/dao/UsersDAO.java
+++ selecao1/src/main/java/br/com/jn/dao/UsersDAO.java
@@ -5,31 +5,29 @@
  */
 package br.com.jn.dao;
 
-import br.com.jn.model.Candidato;
 import br.com.jn.model.Users;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  *
  * @author josmar
  */
-@Stateless
 public class UsersDAO {
 
-    @PersistenceContext(unitName = "SLPU")
-    private EntityManager em;
-
+    //@PersistenceContext(unitName = "SLPU")
+    //private EntityManager em;
     public Boolean adicionar(Users users) {
         try {
             Session session = HibernateUtil.getSessionFactory().openSession();
@@ -71,15 +69,41 @@ public class UsersDAO {
             return false;
         }
     }
-public List<Users> listar() {
-        
+
+    public List<Users> listar() {
+        Authentication a = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = a.getPrincipal();
+        String username = "";
+        username = (((User) a.getPrincipal()).getUsername());
+
+        System.out.println("Username recuperado para recuperar o nome completo e o id {" + username + "}");
+
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
-        Criteria crit = session.createCriteria(Users.class);        
-        crit.addOrder(Order.asc("nome"));
-        
+        Criteria crit = session.createCriteria(Users.class);
+
+        crit.add(Restrictions.eq("username", username));
+
         List<Users> lista = crit.list();
         session.close();
+        for (Users users : lista) {
+            users.setUserId(users.getUserId());
+            users.setNome(users.getNome());
+            System.out.println("Id do Usuario: {" + users.getUserId() + "} Nome Complento: {" + users.getNome() + "}");
+        }
         return lista;
+    }
+
+    public Users procuraUsuario() {
+
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Criteria crit = session.createCriteria(Users.class);
+
+        crit.add(Restrictions.eq("username", "alencar"));
+        
+        Users users = (Users) crit.uniqueResult();
+        session.close();
+        return users;
     }
 }
